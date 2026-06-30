92c1dd5c2f70535c361ffd85120da34f0ca205eb
jayasridhar M <jsdarkdevil.43@gmail.com>
2026-06-30 10:43:01 +0530

    Refactor product grid JavaScript functionality

--- local works.js
+++ local works.js
@@ -80,12 +80,12 @@ function renderCards(dataToRender = dataList) {
         const extraInfo = item.delivery  || item["delivery"]  || Object.values(item)[5] || "";
         const location  = item.location  || item["location"]  || Object.values(item)[6] || "இடம் இல்லை";
 
-        let iconHtml = '<i class="fa-solid fa-box"></i>'; 
-        let typeBadge = 'பிரிவு 1';
+        let iconHtml = '<i class="fa-solid fa-paint-roller"></i>'; 
+        let typeBadge = 'Painters';
         
-        if(type.toString().toLowerCase() === 'cat2') { iconHtml = '<i class="fa-solid fa-tags"></i>'; typeBadge = 'பிரிவு 2'; }
-        if(type.toString().toLowerCase() === 'cat3') { iconHtml = '<i class="fa-solid fa-store"></i>'; typeBadge = 'பிரிவு 3'; }
-        if(type.toString().toLowerCase() === 'cat4') { iconHtml = '<i class="fa-solid fa-star"></i>'; typeBadge = 'பிரிவு 4'; }
+        if(type.toString().toLowerCase() === 'cat2') { iconHtml = '<i class="fa-solid fa-broom"></i>'; typeBadge = 'Cleaners'; }
+        if(type.toString().toLowerCase() === 'cat3') { iconHtml = '<i class="fa-solid fa-truck-ramp-box"></i>'; typeBadge = 'Load Men'; }
+        if(type.toString().toLowerCase() === 'cat4') { iconHtml = '<i class="fa-solid fa-seedling"></i>'; typeBadge = 'Gardeners'; }
 
         card.innerHTML = `
             <div class="card-left">
