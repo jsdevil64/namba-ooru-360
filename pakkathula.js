12a1efb8a62e316a2583c29664bd34ffcf71aaf5
jayasridhar M <jsdarkdevil.43@gmail.com>
2026-06-30 11:21:17 +0530

    Refactor and update product grid functionality

--- pakkathula.js
+++ pakkathula.js
@@ -80,11 +80,11 @@ function renderCards(dataToRender = dataList) {
         const extraInfo = item.delivery  || item["delivery"]  || Object.values(item)[5] || "";
         const location  = item.location  || item["location"]  || Object.values(item)[6] || "இடம் இல்லை";
 
-        let iconHtml = '<i class="fa-solid fa-box"></i>'; 
-        let typeBadge = 'பிரிவு 1';
+        let iconHtml = '<i class="fa-solid fa-faucet"></i>'; 
+        let typeBadge = 'Plumbers';
         
-        if(type.toString().toLowerCase() === 'cat2') { iconHtml = '<i class="fa-solid fa-tags"></i>'; typeBadge = 'பிரிவு 2'; }
-        if(type.toString().toLowerCase() === 'cat3') { iconHtml = '<i class="fa-solid fa-store"></i>'; typeBadge = 'பிரிவு 3'; }
+        if(type.toString().toLowerCase() === 'cat2') { iconHtml = '<i class="fa-solid fa-bolt"></i>'; typeBadge = 'Electricians'; }
+        if(type.toString().toLowerCase() === 'cat3') { iconHtml = '<i class="fa-solid fa-wrench"></i>'; typeBadge = 'Mechanics'; }
         if(type.toString().toLowerCase() === 'cat4') { iconHtml = '<i class="fa-solid fa-star"></i>'; typeBadge = 'பிரிவு 4'; }
 
         card.innerHTML = `
